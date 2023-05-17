842619b9b71490883e9fb6f4a03a3fa77d0386ba
vishal989 <vavishalarya@gmail.com>
2023-05-17 13:13:22 +0530

    "adding item to cart" updated

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 from .models import *
 
 
@@ -34,26 +35,26 @@ def checkout(request):
 
 
 def updateItem(request):
-	# data = json.loads(request.body)
-	# productId = data['productId']
-	# action = data['action']
-	# print('Action:', action)
-	# print('Product:', productId)
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
 
-	# customer = request.user.customer
-	# product = Product.objects.get(id=productId)
-	# order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-	# orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
-	# if action == 'add':
-	# 	orderItem.quantity = (orderItem.quantity + 1)
-	# elif action == 'remove':
-	# 	orderItem.quantity = (orderItem.quantity - 1)
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
 
-	# orderItem.save()
+	orderItem.save()
 
-	# if orderItem.quantity <= 0:
-	# 	orderItem.delete()
+	if orderItem.quantity <= 0:
+		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
